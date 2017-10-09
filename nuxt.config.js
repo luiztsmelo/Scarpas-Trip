54a29d5be9f4171afead44212bcc33a387608bd3
Tarcisio Melo <luiztsmelo@gmail.com>
2017-10-09 13:28:42 -0300

    first commit ma friend

---
+++ nuxt.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  mode: 'spa',
+  /*
+  ** Build configuration
+  */
+  build: {},
+  /*
+  ** Headers
+  ** Common headers are already provided by @nuxtjs/pwa preset
+  */
+  head: {},
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Customize app manifest
+  */
+  loadingIndicator: {
+    name: 'pulse',
+    color: '#0D0D0D',
+    background: 'white'
+  },
+  
+  manifest: {
+    theme_color: '#3B8070'
+  },
+  /*
+  ** Modules
+  */
+  modules: [
+    '@nuxtjs/pwa'
+  ]
+}
