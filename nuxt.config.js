d701bf2f3578d48d17e79d3aedd48537cbde299b
Tarcisio Melo <luiztsmelo@gmail.com>
2017-10-09 14:59:19 -0300

    Design

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ module.exports = {
   head: {
     link: [
       { rel: 'icon', type: 'image/png', sizes: '24x24', href: '/icon.png' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Arizonia' }
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Engagement' }
     ]
   },
   /*
@@ -22,7 +22,7 @@ module.exports = {
   */
   manifest: {
     name: 'Scarpas Trip',
-    theme_color: 'black',
+    theme_color: 'rgb(255, 89, 97)',
     lang: 'pt-br'
   },
   /*
