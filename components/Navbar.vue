0479942c7e86a482ca424ba2f2a85b2a6d998f8e
Tarcisio Melo <luiztsmelo@gmail.com>
2017-10-09 14:39:45 -0300

    Design

---
+++ components/Navbar.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="navbar">
+    <div class="navbar-body">
+
+      <nuxt-link to="/" class="__brand-name">Scarpas Trip</nuxt-link>
+
+      <nav class="nav">
+        <div class="__menu">
+          <div class="bar"></div>
+          <div class="bar"></div>
+          <div class="bar"></div>
+        </div>
+      </nav>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
+
+<style scoped>
+@import url('../assets/css/main.css');
+
+.navbar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 3;
+  height: 3rem;
+  width:  100%;
+  background: white;
+  transition: all .3s ease;
+  padding: 0 6%;
+  box-shadow: 0px 1px 4px 0px rgba(0,0,0,0.1);
+  & .navbar-body {
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    & .__brand-name {
+      font-family: var(--brand-font);
+      font-size: 31px;
+      line-height: 0;
+      user-select: none;
+      font-weight: 400;
+      color: var(--color00);
+      transition: all ease .3s;
+    }
+    & .nav {
+      display: flex;
+      align-items: center;
+      & .__menu {
+        margin: .7rem 0 .7rem .7rem; /* Aumentar área de contato ao pressionar */
+        display: inline-block;
+        cursor: pointer;
+        -webkit-tap-highlight-color: rgba(255, 255, 45, 0);
+        & .bar {
+          transition: all .3s ease;
+          width: 22px;
+          height: 1px;
+          background-color: var(--color00);
+          margin: 5px 0;
+        }
+      }
+    }
+  }
+  
+}
+
+/* TRANSITIONS */
+
+/* .fadeBrandSidebar {
+  transform: translateY(-3.1rem) scale(.5);
+} */
+
+.menuIconAnime1 {
+  transform: rotate(-45deg) translate(-4px, 4px);
+}
+.menuIconAnime2 {
+  opacity: 0;
+  transform: scale(0);
+}
+.menuIconAnime3 {
+  transform: rotate(45deg) translate(-4px, -4px);
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -8,20 +8,24 @@ module.exports = {
   ** Headers
   ** Common headers are already provided by @nuxtjs/pwa preset
   */
-  head: {},
+  head: {
+    link: [
+      { rel: 'icon', type: 'image/png', sizes: '24x24', href: '/icon.png' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Grand+Hotel' }
+    ]
+  },
   /*
   ** Customize the progress-bar color
   */
   loading: { color: '#3B8070' },
-  /*
-  ** Customize app manifest
-  */
   loadingIndicator: {
     name: 'pulse',
     color: '#0D0D0D',
     background: 'white'
   },
-
+  /*
+  ** Customize app manifest
+  */
   manifest: {
     name: 'Scarpas Trip',
     theme_color: '#3B8070',
