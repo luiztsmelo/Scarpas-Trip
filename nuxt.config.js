c06b02a645ac4434911d3b1583e2a899f773f150
Tarcisio Melo <luiztsmelo@gmail.com>
2017-10-09 17:05:57 -0300

    Design

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ module.exports = {
   head: {
     link: [
       { rel: 'icon', type: 'image/png', sizes: '24x24', href: '/icon.png' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900|Dancing+Script:700' }
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900|Poiret+One' }
     ]
   },
   /*
@@ -21,7 +21,6 @@ module.exports = {
   ** Customize app manifest
   */
   manifest: {
-    name: 'Escarpas Trip',
     theme_color: 'black',
     lang: 'pt-br'
   },
