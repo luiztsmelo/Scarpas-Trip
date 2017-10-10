4ea3d2c95e69513d790fec51f3345f6a6bb05181
Tarcisio Melo <luiztsmelo@gmail.com>
2017-10-10 15:43:24 -0300

    Design

--- pages/index.vue
+++ pages/index.vue
@@ -44,7 +44,7 @@ export default {
       text-align: center;
       justify-content: center;
       align-items: center;
-      background: linear-gradient(rgba(4, 12, 31, .85) 40%, rgba(255, 255, 255, 1));
+      background: linear-gradient(rgba(4, 12, 31, .82) 30%, rgba(255, 255, 255, 1));
       padding: 0 6%;
       height: 13rem;
       padding-bottom: 2rem;
