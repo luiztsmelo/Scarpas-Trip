371c8b28616321d1348a89447a3aae78d7193929
Tarcisio Melo <luiztsmelo@gmail.com>
2017-10-19 15:46:56 -0200

    Design

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -53,7 +53,7 @@ export default {
     align-items: center;
     & .__brand-name {
       font-family: var(--brand-font);
-      font-size: 26px;
+      font-size: 27px;
       line-height: 0;
       user-select: none;
       font-weight: 400;
