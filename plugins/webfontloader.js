0d49df3d15ab268f24ceb4ff3781f701cc4ed23e
Tarcisio Melo <luiztsmelo@gmail.com>
2017-11-04 00:18:16 -0200

    Design

--- plugins/webfontloader.js
+++ plugins/webfontloader.js
@@ -2,6 +2,6 @@ import WebFont from 'webfontloader'
 
 WebFont.load({
   google: {
-    families: ['Work Sans:300,400,500,600,700', 'Damion']
+    families: ['Work Sans:300,400,500,600', 'Damion']
   }
 })
