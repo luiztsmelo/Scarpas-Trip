d3fc1b82c96c9517b98de68d335cb99f7e81f3e2
Tarcisio Melo <luiztsmelo@gmail.com>
2017-11-07 14:51:26 -0200

    Design

--- pages/eventos/index.vue
+++ pages/eventos/index.vue
@@ -1,10 +1,22 @@
 <template>
   <div class="eventos">
-    <h1>Eventos</h1>
+    <ul class="eventos-container">
+      <li class="card" v-for="evento in $store.state.eventos" :key="evento.eventoID">
+        <nuxt-link :to="'/eventos/' + evento.eventoID">
+          <img class="__card-img" :src="imageH(evento)" />
+          <span class="__card-date">{{ evento.date }}</span>
+          <h1 class="__card-title">{{ evento.title | truncateTitle }}</h1>
+          <h2 class="__card-subtitle">{{ evento.subtitle | truncateSubtitle }}</h2>
+        </nuxt-link> 
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import supportsWebP from 'supports-webp'
+import * as firebase from 'firebase'
+
 export default {
   head () {
     return {
@@ -14,6 +26,41 @@ export default {
   transition: 'slide-left',
   data () {
     return {
+    }
+  },
+  fetch ({ store }) {
+    if (store.state.eventos === null) {
+      return firebase.database().ref('eventos').once('value')
+      .then(snapshot => {
+        store.commit('m_eventos', snapshot.val())
+      })
+    } else {
+      return console.log('Eventos já carregados')
+    }
+  },
+  methods: {
+    imageH (evento) {
+      if (supportsWebP) {
+        return evento.imageH1W
+      } else {
+        return evento.imageH1J
+      }
+    }
+  },
+  filters: {
+    truncateTitle (value) {
+      if (value.length > 31) {
+        return value.slice(0, 31) + '...'
+      } else {
+        return value.slice(0)
+      }
+    },
+    truncateSubtitle (value) {
+      if (value.length > 26) {
+        return value.slice(0, 26) + '...'
+      } else {
+        return value.slice(0)
+      }
     }
   }
 }
@@ -28,5 +75,33 @@ export default {
   flex-flow: column;
   padding: 3rem 7%;
   transition: all .222s ease;
+  & .eventos-container {
+    padding: 0;
+    display: flex;
+    flex-flow: column;
+    & .card {
+      width: 100%;
+      padding-bottom: 3rem;
+      & .__card-img {
+        margin-bottom: .2rem;
+        width: 100%;
+        height: auto;
+      }
+      & .__card-date {
+        font-size: 14px;
+        font-weight: 600;
+        color: #ff5858;
+      }
+      & .__card-title {
+        margin: .2rem 0;
+        font-size: 26px;
+        font-weight: 600;
+      }
+      & .__card-subtitle {
+        font-size: 20px;
+        font-weight: 300;
+      }
+    }
+  }
 }
 </style>
