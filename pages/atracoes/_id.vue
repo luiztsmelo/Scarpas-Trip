f6d3629109089aba1372fa9074f5f10c3c8cac41
Tarcisio Melo <luiztsmelo@gmail.com>
2018-01-30 14:34:09 -0200

    Design

--- pages/atracoes/_id.vue
+++ pages/atracoes/_id.vue
@@ -118,16 +118,17 @@ export default {
       },
       markerIconPartida: {
         url: 'https://firebasestorage.googleapis.com/v0/b/escarpas-trip.appspot.com/o/utils%2Fmarker-partida.svg?alt=media&token=bd41c89e-33ea-4899-bb5a-4f2fc2d936cb',
-        scaledSize: new google.maps.Size(36, 36)
+        scaledSize: new google.maps.Size(34, 34)
       },
       markerIconChegada: {
         url: 'https://firebasestorage.googleapis.com/v0/b/escarpas-trip.appspot.com/o/utils%2Fmarker-chegada.svg?alt=media&token=b5b52bc5-a65f-4136-9c31-57830b969067',
-        scaledSize: new google.maps.Size(36, 36)
+        scaledSize: new google.maps.Size(34, 34)
       },
       polylineOptions: {
-        strokeColor: '#1F875A',
-        strokeOpacity: 0.7
-      },
+        strokeColor: '#3e3e3e',
+        strokeOpacity: 1,
+        strokeWeight: 2
+      }
     }
   },
   head () {
