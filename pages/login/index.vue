9b0eda37385ca1b5a56ce0a1ffbe9d5b50f90a4f
Tarcisio Melo <luiztsmelo@gmail.com>
2018-02-07 17:38:46 -0200

    Design

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="login">
 
-    <img src="../../assets/img/social.svg" class="__img-header">
-    <h1 class="__title">Continuar com:</h1>
+    <img src="../../assets/img/login.svg" class="__img-header">
+    <h1 class="__title">Continuar com sua conta:</h1>
 
     <div class="signin-btns">
       <button type="button" class="facebook-btn" @click="facebookSignIn()">Facebook</button>
@@ -33,8 +33,9 @@ export default {
     height: auto;
   }
   & .__title {
-    font-size: 28px;
+    font-size: 26px;
     font-weight: 700;
+    padding: 0 7%;
     text-align: center;
   }
   & .signin-btns {
