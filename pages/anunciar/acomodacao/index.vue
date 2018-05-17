9a68c3df5eb7ee28d6008452712ff3f096b93635
Tarcisio Melo <luiztsmelo@gmail.com>
2018-05-17 16:24:18 -0300

    Design

--- pages/anunciar/acomodacao/index.vue
+++ pages/anunciar/acomodacao/index.vue
@@ -6,23 +6,12 @@
     <!-- PLANO ACOMODAÇÃO -->
     <div class="plano-acomodacao" v-show="$store.state.cadastroAcomod0">
       
-      <img class="__img-header" src="../../../assets/img/estadia.svg">
+      <img class="__img-header" src="../../../assets/img/anuncio-acomod.svg">
        
       <h1 class="__title">Amplie seus ganhos anunciando sua casa com a gente!</h1>
       
-      <span class="__subtitle">Como funciona?</span>
-      <h3 class="__item">A Escarpas Trip </h3>
 
 
-      <span class="__subtitle">Quais as vantagens?</span>
-      <h3 class="__item">Destaque nas redes sociais</h3>
-      <h3 class="__item">Transparência</h3>
-      <h3 class="__item">Segurança</h3>
-      
-      <span class="__subtitle">Investimento Mensal</span>
-      <span class="__price">R$49</span>
-      <h3 class="__subitem">Primeiro mês grátis</h3>
-      <h3 class="__subitem">Cancele quando quiser</h3>
 
       <button class="__anunciar-btn" @click="$store.commit('m_cadastroAcomod1', true), $store.commit('m_cadastroAcomod0', false), $store.commit('m_acomodProgressBar', (100/11)), hashAcomod()">Anunciar</button>
 
@@ -1371,8 +1360,6 @@ export default {
 
 .anunciar-acomodacao {
   margin-top: 3.2rem;
-  background: var(--colorAcomod);
-  color: white;
   transition: var(--main-transition);
   & .progress-bar {
     position: fixed;
@@ -1388,55 +1375,26 @@ export default {
     flex-flow: column;
     align-items: center;
     & .__img-header {
-      margin: 2rem 0;
-      width: 6rem;
+      margin: 3rem 0 2rem 0;
+      width: 7rem;
       height: auto;
-      filter: grayscale(100%) brightness(250%);
     }
     & .__title {
-      font-size: 26px;
-      font-weight: 700;
+      font-size: 28px;
+      font-weight: 500;
       padding: 0 7%;
       text-align: center;
-    }
-    & .__subtitle {
-      text-transform: uppercase;
-      margin: 3rem 0 .5rem 0;
-      font-size: 15px;
-      font-weight: 700;
-      padding: 0 7%;
-      text-align: center;
-    }
-    & .__item {
-      padding: 0 7%;
-      font-size: 18px;
-      font-weight: 400;
-      margin: 0 0 .2rem 0;
-      color: white;
-    }
-    & .__subitem {
-      padding: 0 7%;
-      font-size: 14px;
-      font-weight: 400;
-      margin: 0 ;
-      color: white;
-      line-height: 21px;
-    }
-    & .__price {
-      font-size: 52px;
-      font-weight: 100;
-      margin-bottom: .5rem;
     }
     & .__anunciar-btn {
-      color: var(--color01);
-      width: 65%;
-      background: white;
-      padding: 1rem 0;
-      border-radius: 100px;
+      color: white;
+      width: 13rem;
+      background: var(--colorAcomod);
+      height: 2.85rem;
+      border-radius: 2rem;
       font-size: var(--fontSizeAnuncioText);
       font-weight: 600;
       margin: 3rem 0 2rem 0;
-      box-shadow: 2px 2px 9px 4px rgba(0,0,0,0.13);
+      box-shadow: 3px 3px 20px 1px rgba(0,0,0,0.18);
     }
   }
   /* ******************** CADASTRO ACOMODAÇÃO ******************** */
@@ -1695,16 +1653,9 @@ export default {
     }
     & .plano-acomodacao {
     & .__img-header {
+      width: 7rem;
     }
     & .__title {
-    }
-    & .__subtitle {
-    }
-    & .__item {
-    }
-    & .__subitem {
-    }
-    & .__price {
     }
     & .__anunciar-btn {
     }
--- pages/anunciar/index.vue
+++ pages/anunciar/index.vue
@@ -6,22 +6,22 @@
     <div class="category-container">
 
       <nuxt-link to="/anunciar/acomodacao" class="card">
-        <img src="../../assets/img/casa.jpg" class="__card-img">
+        <img src="../../assets/img/anuncio-acomod.svg" class="__card-img">
         <button class="__card-btn">Acomodação</button>
       </nuxt-link>
 
       <nuxt-link to="/anunciar/passeio" class="card">
-        <img src="../../assets/img/passeio.jpg" class="__card-img">
+        <img src="../../assets/img/anuncio-passeio.svg" class="__card-img">
         <button class="__card-btn">Passeio</button>
       </nuxt-link>
 
       <nuxt-link to="/anunciar/evento" class="card">
-        <img src="../../assets/img/evento.jpg" class="__card-img">
+        <img src="../../assets/img/anuncio-evento.svg" class="__card-img">
         <button class="__card-btn">Evento</button>
       </nuxt-link>
 
       <nuxt-link to="/anunciar/restaurante" class="card">
-        <img src="../../assets/img/restaurante.jpg" class="__card-img">
+        <img src="../../assets/img/anuncio-restaurante.svg" class="__card-img">
         <button class="__card-btn">Restaurante</button>
       </nuxt-link>
 
@@ -50,9 +50,9 @@ export default {
   align-items: center;
   transition: var(--main-transition);
   & .__title {
-    padding: 4rem 20% 4rem;
+    padding: 4rem 20% 5rem;
     font-size: 34px;
-    font-weight: 700;
+    font-weight: 600;
     text-align: center;
     line-height: 45px;
   }
@@ -65,17 +65,15 @@ export default {
       display: flex;
       flex-flow: column;
       align-items: center;
-      width: 23%;
-      border-radius: 3px;
-      box-shadow: 2px 2px 9px 4px rgba(0,0,0,0.13);
+      width: 17%;
       & .__card-img {
-        width: 100%;
+        width: 55%;
         height: auto;
       }
       & .__card-btn {
         width: 100%;
-        height: 2.9rem;
-        font-size: 16px;
+        height: 4rem;
+        font-size: 20px;
         font-weight: 600;
         background: white;
       }
