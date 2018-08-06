4c7df74f93424e09b41735d01924b882a515869a
Tarcisio Melo <luiztsmelo@gmail.com>
2018-08-06 17:04:18 -0300

    fixes

--- pages/anunciar/acomodacao/index.vue
+++ pages/anunciar/acomodacao/index.vue
@@ -754,7 +754,6 @@ export default {
       isUploading: false,
       uploadProgress: 0,
       newRegra: '',
-      monthsPermitted: ['01','02','03','04','05','06','07','08','09','10','11','12'],
       bankCodeError: false,
       agenciaError: false,
       agenciaDVError: false,
